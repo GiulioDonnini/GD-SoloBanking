2c9bb8deb1cce49facd382cb866740c9f70fabd6
Giulio <giuliodonnini00@gmail.com>
2025-11-13 16:37:11 +0100

    ADD GlobalExceptionHandler

---
+++ backend/src/main/java/it/solobanking/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package it.solobanking.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> gestisciErroreGenerico(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
