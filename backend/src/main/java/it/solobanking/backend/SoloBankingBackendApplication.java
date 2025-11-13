cc49d0e3cea5cd53f22baaed82b1e371e8d0b6a0
Giulio <giuliodonnini00@gmail.com>
2025-11-13 12:32:03 +0100

    Commit Iniziale

---
+++ backend/src/main/java/it/solobanking/backend/SoloBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package it.solobanking.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloBankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloBankingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/solobanking/backend/SoloBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.solobanking.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloBankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
