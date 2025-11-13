a88dba9235ef30f2f1163e934bc335b98e1318eb
Giulio <giuliodonnini00@gmail.com>
2025-11-13 21:11:38 +0100

    Aggiunta bonifico.component

---
+++ frontend/src/app/pages/bonifico/bonifico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonifico',
+  standalone: true,
+  imports: [],
+  templateUrl: './bonifico.component.html',
+  styleUrl: './bonifico.component.css'
+})
+export class BonificoComponent {
+
+}
