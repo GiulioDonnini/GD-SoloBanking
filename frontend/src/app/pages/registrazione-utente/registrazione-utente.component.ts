2b929a8ea2d129e99a4e4efd0d5d68bbc1fd12a9
Giulio <giuliodonnini00@gmail.com>
2025-11-14 10:26:09 +0100

    ADD componente di registrazione per Utente

---
+++ frontend/src/app/pages/registrazione-utente/registrazione-utente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrazione-utente',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrazione-utente.component.html',
+  styleUrl: './registrazione-utente.component.css'
+})
+export class RegistrazioneUtenteComponent {
+
+}
